19e4a3715904d95690517b36b3f6d6aab653d181
ysuzuki19 <42496938+ysuzuki19@users.noreply.github.com>
2023-10-25 17:42:07 +0900

    add test for os environment

---
+++ tests/case_enveror_osenv.rs
@@ -0,0 +1,29 @@
+use std::path::PathBuf;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "UPPERCASE")]
+struct EnverorConfig {
+    programatic_bool: bool,
+    programatic_int: i32,
+    programatic_str: String,
+    programatic_arr: Vec<i32>,
+}
+
+extern crate enveror;
+
+#[test]
+fn parse_deserialize() -> Result<(), Box<dyn std::error::Error>> {
+    std::env::set_var("PROGRAMATIC_BOOL", "true");
+    std::env::set_var("PROGRAMATIC_INT", "1");
+    std::env::set_var("PROGRAMATIC_STR", "SETTED");
+    std::env::set_var("PROGRAMATIC_ARR", "[1,2,3]");
+
+    enveror::Enveror::new()
+        .ignore_default_config()
+        .path(PathBuf::from("./tests/case_enveror_local"))
+        .load()?
+        .construct::<EnverorConfig>()?;
+    Ok(())
+}
