d532d1fb282051753f253b11272bb848ecfb6b1a
ysuzuki19 <ysuzuki.cpp@gmail.com>
2025-10-17 03:25:44 +0900

    fix by linter

--- src/tree/string_into_json/mod.rs
+++ src/tree/string_into_json/mod.rs
@@ -6,14 +6,14 @@ use crate::into_json::IntoJson;
 use self::modifier::Modifier;
 use self::type_checker::TypeChecker;
 
-impl<'a> IntoJson for &'a str {
+impl IntoJson for &str {
     fn into_json(self) -> String {
         if self.is_vec() || self.is_bool() || self.is_number() {
             self.into()
         } else {
             let modified = self.trim_quotes().escape();
 
-            format!("\"{}\"", modified)
+            format!("\"{modified}\"")
         }
     }
 }
